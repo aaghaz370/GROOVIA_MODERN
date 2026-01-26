f4de956fdc1f7cf73c318e4a9d62108fa6578ccc
aaghaz afzal <aghazafzal8294@gmail.com>
2026-01-26 18:23:17 +0530

    Search header - negative margins to force screen edge touch

--- FRONTEND/src/app/search/page.tsx
+++ FRONTEND/src/app/search/page.tsx
@@ -169,7 +169,7 @@ export default function SearchPage() {
             <div className="pt-6 pb-4 md:px-6 bg-sidebar sticky top-0 z-30">
                 <div className="max-w-7xl mx-auto">
                     {/* Search Input */}
-                    <div className="relative mb-4">
+                    <div className="relative mb-4 -mx-4 px-4">
                         <BiSearch className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" size={24} />
                         <input
                             type="text"
@@ -183,7 +183,7 @@ export default function SearchPage() {
 
                     {/* Filter Tabs */}
                     {query && (
-                        <div className="flex gap-2 overflow-x-auto scrollbar-hide">
+                        <div className="flex gap-2 overflow-x-auto scrollbar-hide -mx-4 px-4">
                             {filters.map((filter) => (
                                 <button
                                     key={filter.id}
